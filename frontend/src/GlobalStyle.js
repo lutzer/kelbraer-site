995a47b90418dc248296acca24f677c26660ba9f
Tuukka Kantola <t@tuukka.is>
2020-12-12 21:04:20 +0100

    Add padding to the main container and more font-size variables

--- frontend/src/GlobalStyle.js
+++ frontend/src/GlobalStyle.js
@@ -2,8 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     :root {
-        --s1: 26.6px;
+        --s3: 47px;
+        --s2: 36px;
+        --s1: 27px;
         --s0: 20px;
+
+        --contentPadding: 30px;
     }
 
   body {
@@ -13,6 +17,10 @@ const GlobalStyle = createGlobalStyle`
     font-size: var(--s0);
     font-family: Open-Sans, Helvetica, Sans-Serif;
     overflow: scroll;
+  }
+
+  h1 {
+    font-size: var(--s3);
   }
 `;
 
