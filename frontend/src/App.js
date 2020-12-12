c24323932b5c2b89ab48b40f453fda8e90bcef73
Tuukka Kantola <t@tuukka.is>
2020-12-12 20:33:09 +0100

    Add intro component, remove unused css file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
 import styled from "styled-components"
 import { useRef } from "react"
 import Message from "./components/Message"
+import Intro from "./components/Intro"
 
 function App() {
   return (
     <div className="App">
-    <header>
-      <h1>Welcome to the website of Kelbraer people</h1>
-    </header>
-    <main>
-      <Message></Message>
-    </main>
+      <Intro></Intro>
+      <main>
+        <Message></Message>
+      </main>
     </div>
   );
 }
