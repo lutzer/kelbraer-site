bc6ce0371bd3f1fab10373d341d463df77fb1b6a
Tuukka Kantola <t@tuukka.is>
2020-12-12 19:09:53 +0100

    Revert "Add eslint"
    This reverts commit 200aed74b7e49dcab2b11fb522c17b3f738dab6c.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Welcome to the website of Kelbraer people</h1>
+    <header>
+      <h1>Welcome to the website of Kelbraer people</h1>
       </header>
-      <main>
-        <h2>Send a message to us</h2>
-        <textarea />
+      <main><h2>Send a message to us</h2>
+        <textarea></textarea>
         <button>Send message</button>
       </main>
     </div>
