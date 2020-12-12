a3778971bdd0bf28bbcf33e5b2cab9ace26c1921
Tuukka Kantola <t@tuukka.is>
2020-12-12 15:03:55 +0100

    Add front page skeleton and update gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <header>
+      <h1>Welcome to the website of Kelbraer people</h1>
       </header>
+      <main><h2>Send a message to us</h2>
+        <textarea></textarea>
+        <button>Send message</button>
+      </main>
     </div>
   );
 }
