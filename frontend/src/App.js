200aed74b7e49dcab2b11fb522c17b3f738dab6c
Tuukka Kantola <t@tuukka.is>
2020-12-12 18:26:06 +0100

    Add eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <header>
-      <h1>Welcome to the website of Kelbraer people</h1>
+      <header>
+        <h1>Welcome to the website of Kelbraer people</h1>
       </header>
-      <main><h2>Send a message to us</h2>
-        <textarea></textarea>
+      <main>
+        <h2>Send a message to us</h2>
+        <textarea />
         <button>Send message</button>
       </main>
     </div>
