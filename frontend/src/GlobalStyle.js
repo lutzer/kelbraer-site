1b2ae638b43d76151c99dee4b18baca87ee8d3c5
Tuukka Kantola <t@tuukka.is>
2020-12-12 23:04:15 +0100

    Add Rubik font

--- frontend/src/GlobalStyle.js
+++ frontend/src/GlobalStyle.js
@@ -1,22 +1,48 @@
 import { createGlobalStyle } from 'styled-components';
+import RubikRegular from './fonts/Rubik-Regular.ttf';
+import RubikBold from './fonts/Rubik-Bold.ttf';
 
 const GlobalStyle = createGlobalStyle`
-    :root {
+  @font-face {
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: normal;
+    src: url(${RubikRegular}) format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Rubik';
+    font-style: normal;
+    font-weight: bold;
+    src: url(${RubikBold}) format('truetype');
+  }
+
+
+  :root {
         --s3: 47px;
         --s2: 36px;
         --s1: 27px;
         --s0: 20px;
 
         --contentPadding: 30px;
-    }
+
+        --black: #25221D;
+        --red: #F05624;
+        --orange: #F4A811;
+        --beige: #FFF5DC;
+        --white: #fff;
+        
+        --bg: var(--beige);
+  }
 
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     font-size: var(--s0);
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+    font-family: Rubik, sans-Serif;
     overflow: scroll;
+    background-color: var(--bg);
   }
 
   h1 {
--- frontend/src/components/Intro.js
+++ frontend/src/components/Intro.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import {motion} from "framer-motion"
 
 function Intro() {
 
@@ -7,11 +8,20 @@ function Intro() {
     background-color: #FFF5DC;
     padding: 40px;
     text-align: center;
+    overflow: hidden;
   `
+
+  const Cloud = styled(motion.div)`
+    width: 200px;
+    height: 30px;
+    background-color: var(--red);
+  `
+
 
   return (
     <Container>
         <h1>Welcome to the website of Kelbraer people</h1>
+        <Cloud animate={{x: "120vw"}} transition={{ duration: 10 }}></Cloud>
     </Container>
   );
 }
